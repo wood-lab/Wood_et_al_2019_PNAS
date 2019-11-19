d17e22b071a1df39998d486c8a92434b5685c28f
Grant Adams <adamsgd@uw.edu>
2019-11-18 16:18:31 -0800

    Removed old runs for analysis 3

--- src/analysis_3_habitat_correlates_of_snail_density/Report/Final_run_script.R
+++ src/analysis_3_habitat_correlates_of_snail_density/Report/Final_run_script.R
@@ -15,7 +15,7 @@ fit_mod(model = 2, incl_disease = 0, bulinus = T, BIC_sel = TRUE, fill_cont_mean
 
 
 # Without outlier
-fit_mod(model = 2, incl_disease = 0, bulinus = T, BIC_sel = TRUE, fill_cont_mean = F, fill_cat_mode = F, remove_outlier = TRUE, method = "nlminb", silent = TRUE, savedir = "Report/Final_run") # Bulinus delta-pl
-fit_mod(model = 2, incl_disease = 0, bulinus = T, BIC_sel = TRUE, fill_cont_mean = F, fill_cat_mode = T, remove_outlier = TRUE, method = "nlminb", silent = TRUE, savedir = "Report/Final_run") # Bulinus delta-pl
-fit_mod(model = 2, incl_disease = 0, bulinus = T, BIC_sel = TRUE, fill_cont_mean = T, fill_cat_mode = F, remove_outlier = TRUE, method = "nlminb", silent = TRUE, savedir = "Report/Final_run") # Bulinus delta-pl
-fit_mod(model = 2, incl_disease = 0, bulinus = T, BIC_sel = TRUE, fill_cont_mean = T, fill_cat_mode = T, remove_outlier = TRUE, method = "nlminb", silent = TRUE, savedir = "Report/Final_run") # Bulinus delta-pl
+fit_mod(model = 2, incl_disease = 0, bulinus = T, BIC_sel = TRUE, fill_cont_mean = F, fill_cat_mode = F, remove_outlier = TRUE, method = "nlminb", silent = TRUE, savedir = "Report/Final_run_no_outlier") # Bulinus delta-pl
+fit_mod(model = 2, incl_disease = 0, bulinus = T, BIC_sel = TRUE, fill_cont_mean = F, fill_cat_mode = T, remove_outlier = TRUE, method = "nlminb", silent = TRUE, savedir = "Report/Final_run_no_outlier") # Bulinus delta-pl
+fit_mod(model = 2, incl_disease = 0, bulinus = T, BIC_sel = TRUE, fill_cont_mean = T, fill_cat_mode = F, remove_outlier = TRUE, method = "nlminb", silent = TRUE, savedir = "Report/Final_run_no_outlier") # Bulinus delta-pl
+fit_mod(model = 2, incl_disease = 0, bulinus = T, BIC_sel = TRUE, fill_cont_mean = T, fill_cat_mode = T, remove_outlier = TRUE, method = "nlminb", silent = TRUE, savedir = "Report/Final_run_no_outlier") # Bulinus delta-pl
